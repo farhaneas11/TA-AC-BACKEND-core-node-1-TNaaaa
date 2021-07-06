1c4dadef81704f298e3af4d73a6d37a29637de12
farhaneas11 <farhan.salam11@gmail.com>
2021-07-07 00:37:31 +0530

    node-js-6

---
+++ block-BNaaar/index.js
@@ -0,0 +1,31 @@
+var http = require('http');
+var server = http.createServer(handlerequest);
+var url = require('url');
+
+function handlerequest(req,res){
+    var parseurl = url.parse(req.url);
+    var pathurl = parseurl.pathname;
+
+    console.log(req.method,req.url,parseurl );
+
+    if(req.method === 'GET' && req.url === '/'){
+        res.setHeader('content-type' , 'text/html');
+        res.end('<h4>welcome to index page</h4>')
+    }
+    else if(req.method === 'GET' && pathurl === '/users'){
+        res.setHeader('content-type' , 'text/html');
+        res.end('<h3>users page</h3>')
+    }
+    else if(req.method === 'GET' && pathurl === '/about'){
+        res.setHeader('content-type' , 'text/html');
+        res.end('<h1>About page</h1>')
+    }
+    else{
+        res.writeHead(404,{'content-type' : 'text/html'});
+        res.end('<h2>About page</h2>')
+    }
+}
+
+server.listen(3000,()=>{
+    console.log('server created');
+})
